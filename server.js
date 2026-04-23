79ca679e1b27dd39b13ac77a4e86e96ae1cf7de9
Lakhan Singh <lakhansingh4668@gmail.com>
2026-04-23 21:23:08 +0530

    Day 15: Added API security, rate limiting, helmet, and fixed Redis env config

--- server.js
+++ server.js
@@ -3,23 +3,37 @@ dotenv.config();
 
 import app from "./src/app.js";
 import { connectDB } from "./src/config/db.js";
-import { connectRedis } from "./src/config/redis.js";
+
+const { connectRedis } = await import("./src/config/redis.js");
+
 import { createServer } from "http";
 import { Server } from "socket.io";
 
+import helmet from "helmet";
+
+// security middleware
+app.use(helmet());
+
+// create HTTP server
 const server = createServer(app);
+
+// socket setup
 const io = new Server(server, {
   cors: { origin: "*" }
 });
 
 global.io = io;
 
-connectDB();
-await connectRedis();
-
+// socket connection
 io.on("connection", (socket) => {
   console.log("Client connected");
 });
+
+// connect databases
+connectDB();
+await connectRedis();
+
+// start server
 server.listen(process.env.PORT, () => {
   console.log(`Server running on port ${process.env.PORT}`);
 });
